d6641cefdbe745dacea4c4e882289704b492dc83
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-11-24 19:22:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SbicktRails::Application.config.secret_token = 'cb51c1909a152e2e22c47b58ec0e551f7171cd5ba5f27babfa99a1a89fb0d90c4149c6d9a8809a7b30f7aa3184f9ab4a5962e73dda6257695042e71cc09362fc'
