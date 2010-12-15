ed9f52188ce02920ca98cedafdf28567b139c2fc
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-12-15 15:29:32 +0100

    added form for insertion of dummy data

--- app/controllers/geotags_controller.rb
+++ app/controllers/geotags_controller.rb
@@ -4,9 +4,23 @@ class GeotagsController < ApplicationController
     @geotags = Geotag.all
   
     respond_to do |format|
-	  #format.html
-	  format.kml
-	end
+	    format.html
+	    format.kml
+    end
+  end
+  
+  def new
+    @new_geotag = Geotag.new
   end
 
+  def create
+    @new_geotag = Geotag.new(params[:geotag])
+      
+    if @new_geotag.save
+      redirect_to geotags_path
+    end
+  end
+  
+  def show
+  end
 end
