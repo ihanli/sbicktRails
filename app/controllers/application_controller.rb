73690d8f33d6516f97635b0c34c6c372e02b0ddc
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-01-09 21:24:18 +0100

    added copying permission statement to new files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
+#################################################
+#            s'bickt Android Client             #
+# Bildungseinrichtung:  Fachhochschule Salzburg #
+#         Studiengang:  MultiMediaTechnology    #
+#               Zweck:  Qualifikationsprojekt   #
+#################################################################################
+#                                                                               #
+# This is the client for the augmented reality and social community app s'bickt #
+# Copyright Alexander Flatscher, Ismail Hanli                                   #
+#                                                                               #
+# This file is part of s'bickt.                                                 #
+#                                                                               #
+# S'bickt is free software: you can redistribute it and/or modify               #
+# it under the terms of the GNU Affero General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or             #
+# (at your option) any later version.                                           #
+#                                                                               #
+# S'bickt is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 #
+# GNU Affero General Public License for more details.                           #
+#                                                                               #
+# You should have received a copy of the GNU Affero General Public License      #
+# along with s'bickt.  If not, see <http://www.gnu.org/licenses/>.              #
+#################################################################################
+
 class ApplicationController < ActionController::Base
 #  protect_from_forgery
   
